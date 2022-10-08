654e51cc99b5ade83abff29edb54cab5a8263132
nariyakitada <narippe002@icloud.com>
2022-10-08 16:20:46 +0900

    編集機能、削除機能

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -22,6 +22,32 @@ class PrototypesController < ApplicationController
     @prototype = Prototype.find(params[:id])
   end
 
+
+  def update
+    @prototype = Prototype.new(prototype_params)
+
+    prototype = Prototype.find(params[:id])
+    
+    if prototype.update(prototype_params)
+      redirect_to prototype_path
+    else
+      render :edit
+    end
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+    unless @prototype.user_id == current_user.id
+      redirect_to action: :index
+    end
+  end
+
+  def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+    redirect_to root_path
+  end
+
   private
 
   def prototype_params
